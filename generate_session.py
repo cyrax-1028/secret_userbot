05ebfde5eb8b15f87309b8700b1ccecda495d244
Bahromjon <bahromjonsherqoziyev@gmail.com>
2025-03-11 17:53:28 +0500

    autoreply

--- generate_session.py
+++ generate_session.py
@@ -1,13 +1,13 @@
-# import os
-# from telethon.sync import TelegramClient
-# from telethon.sessions import StringSession
-# from dotenv import load_dotenv
-#
-# load_dotenv()
-#
-# API_ID = os.getenv("API_ID")
-# API_HASH =os.getenv("API_HASH")
-#
-# with TelegramClient(StringSession(), API_ID, API_HASH) as client:
-#     print("\n✅ Yangi STRING_SESSION yaratildi:")
-#     print(client.session.save())
+import os
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = os.getenv("API_ID")
+API_HASH =os.getenv("API_HASH")
+
+with TelegramClient(StringSession(), API_ID, API_HASH) as client:
+    print("\n✅ Yangi STRING_SESSION yaratildi:")
+    print(client.session.save())
